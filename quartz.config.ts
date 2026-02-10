9586520163e90925ee33119ae6b938575efdf7af
Ethan Pham <ethaniscool135@gmail.com>
2026-02-10 00:47:05 -0800

    Quartz sync: Feb 10, 2026, 12:47 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "github.com/ethaniscool135/obsidian-website.git",
+    baseUrl: "ethaniscool135.github.io/obsidian-website",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
